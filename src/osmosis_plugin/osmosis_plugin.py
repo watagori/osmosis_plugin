05050683ff642802cebfa9579e6669db1786f342
watagori <cottongorillla@gmail.com>
2022-01-21 02:10:55 +0000

    add can_handle

--- src/osmosis_plugin/osmosis_plugin.py
+++ src/osmosis_plugin/osmosis_plugin.py
@@ -3,7 +3,15 @@ from senkalib.caaj_plugin import CaajPlugin
 from senkalib.caaj_journal import CaajJournal
 
 
-class OsmosisPlugin:
+class OsmosisPlugin(CaajPlugin):
     @classmethod
-    def can_handle(cls):
-        return True
+    def can_handle(cls, transaction) -> bool:
+        chain_type = transaction.get_transaction()['header']['chain_id']
+        if "osmosis" in chain_type:
+            return True
+        else:
+            return False
+
+    @classmethod
+    def get_caajs(cls, transaction) -> CaajJournal:
+        pass
--- tests/test_osmosis_plugin.py
+++ tests/test_osmosis_plugin.py
@@ -1,7 +1,23 @@
+import json
 from senkalib.chain.osmosis.osmosis_transaction import OsmosisTransaction
-from src.osmosis_plugin.osmosis_plugin import OsmosisPlugin
+from osmosis_plugin.osmosis_plugin import OsmosisPlugin
 
 
 class TestOsmosisPlugin():
     def test_can_handle_00(self):
-        pass
+        test_data = TestOsmosisPlugin.__get_test_data("swap")
+        transaction = OsmosisTransaction(test_data)
+        chain_type = OsmosisPlugin.can_handle(transaction)
+        assert chain_type
+
+    def test_can_handle_01(self):
+        test_data = TestOsmosisPlugin.__get_test_data("cosmos_transfer")
+        transaction = OsmosisTransaction(test_data)
+        chain_type = OsmosisPlugin.can_handle(transaction)
+        assert chain_type is False
+
+    @classmethod
+    def __get_test_data(cls, filename):
+        with open(f"tests/data/{filename}.json", encoding="utf-8") as jsonfile_local:
+            test_data = json.load(jsonfile_local)
+        return test_data
