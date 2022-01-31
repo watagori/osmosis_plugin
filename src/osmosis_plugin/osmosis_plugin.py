aa6551913b2282593fb78e103bf80af0b9387334
watagori <cottongorillla@gmail.com>
2022-01-31 06:30:26 +0000

    fix int to string

--- src/osmosis_plugin/osmosis_plugin.py
+++ src/osmosis_plugin/osmosis_plugin.py
@@ -45,18 +45,18 @@ class OsmosisPlugin(CaajPlugin):
       elif transaction_type == "MsgBeginUnlocking":
         pass
 
-      if transaction.get_transaction_fee() != 0 and caaj_main:
+      if transaction.get_transaction_fee() != "0" and caaj_main:
         caaj_fee = OsmosisPlugin.__get_caaj_fee(transaction, address)
         print("transaction.get_transaction_fee() !=0 and caaj_main:")
         return [caaj_main, caaj_fee]
 
-      elif transaction.get_transaction_fee() != 0 and caaj_main is None:
+      elif transaction.get_transaction_fee() != "0" and caaj_main is None:
         print("transaction.get_transaction_fee() != 0 and caaj_main:")
 
         caaj_fee = OsmosisPlugin.__get_caaj_fee(transaction, address)
         return [caaj_fee]
 
-      elif transaction.get_transaction_fee() == 0 and caaj_main:
+      elif transaction.get_transaction_fee() == "0" and caaj_main:
         print("transaction.get_transaction_fee() != 0 and caaj_main:")
 
         return [caaj_main]
