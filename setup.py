47758649b2ffc2229cf031c62afb5f3803422593
watagori <cottongorillla@gmail.com>
2022-02-11 02:00:59 +0000

    feat change indent 2

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='osmosis_plugin',
-    version='0.0.1',
-    license='mit',
-    description='plugin for osmosis',
+  name='osmosis_plugin',
+  version='0.0.1',
+  license='mit',
+  description='plugin for osmosis',
 
-    author='watagori',
-    url='https://github.com/ca3-caaip/ca3-caaip',
+  author='watagori',
+  url='https://github.com/ca3-caaip/ca3-caaip',
 
-    extras_require={
-        "test": ["pytest", "pytest-cov", "senkalib"]
-    },
+  extras_require={
+      "test": ["pytest", "pytest-cov", "senkalib"]
+  },
 
-    packages=find_packages('src'),
-    package_dir={'': 'src'}
+  packages=find_packages('src'),
+  package_dir={'': 'src'}
 )
