3ec1d8f4ccec6f529c39f0b0cf0f687ccccfa55b
yuma300 <ywakimoto2s@gmail.com>
2022-04-07 11:37:20 +0900

    make get_caaj use TokenOriginalIdTable

--- tests/test_osmosis_plugin.py
+++ tests/test_osmosis_plugin.py
@@ -1,8 +1,16 @@
+import csv
 import json
+from unittest.mock import patch
 
 from senkalib.chain.osmosis.osmosis_transaction import OsmosisTransaction
 
 from osmosis_plugin.osmosis_plugin import OsmosisPlugin
+from senkalib.senka_lib import SenkaLib
+
+import pandas as pd
+import os
+from pathlib import Path
+from unittest.mock import MagicMock
 
 
 class TestOsmosisPlugin:
@@ -23,9 +31,11 @@ class TestOsmosisPlugin:
     def test_get_caajs_fee_not_zero(self):
         test_data = TestOsmosisPlugin._get_test_data("ibc_transfer")
         transaction = OsmosisTransaction(test_data)
+        mock = TestOsmosisPlugin.get_token_table_mock()
         caajs = OsmosisPlugin.get_caajs(
             "osmo14ls9rcxxd5gqwshj85dae74tcp3umypp786h3m",
             transaction,
+            mock
         )
         assert caajs[1].executed_at == "2022-02-08 01:43:31"
         assert caajs[1].chain == "osmosis"
@@ -46,18 +56,22 @@ class TestOsmosisPlugin:
     def test_get_caajs_fee_zero(self):
         test_data = TestOsmosisPlugin._get_test_data("swap")
         transaction = OsmosisTransaction(test_data)
+        mock = TestOsmosisPlugin.get_token_table_mock()
         caajs = OsmosisPlugin.get_caajs(
             "osmo14ls9rcxxd5gqwshj85dae74tcp3umypp786h3m",
             transaction,
+            mock
         )
         assert len(caajs) == 2
 
     def test_get_caajs_swap_get(self):
         test_data = TestOsmosisPlugin._get_test_data("swap")
         transaction = OsmosisTransaction(test_data)
+        mock = TestOsmosisPlugin.get_token_table_mock()
         caajs = OsmosisPlugin.get_caajs(
             "osmo14ls9rcxxd5gqwshj85dae74tcp3umypp786h3m",
             transaction,
+            mock
         )
 
         assert caajs[0].executed_at == "2022-01-21 02:47:05"
@@ -104,19 +118,21 @@ class TestOsmosisPlugin:
     def test_get_caaj_transfer(self):
         test_data = TestOsmosisPlugin._get_test_data("ibc_transfer")
         transaction = OsmosisTransaction(test_data)
+        mock = TestOsmosisPlugin.get_token_table_mock()
         caajs = OsmosisPlugin.get_caajs(
             "osmo14ls9rcxxd5gqwshj85dae74tcp3umypp786h3m",
             transaction,
+            mock
         )
         assert caajs[0].executed_at == "2022-02-08 01:43:31"
         assert caajs[0].chain == "osmosis"
         assert caajs[0].platform == "osmosis"
-        assert caajs[0].application == "transfer"
+        assert caajs[0].application == "osmosis"
         assert (
             caajs[0].transaction_id
             == "AD666E0F5FB2D7DEF4B09E7BC31AF4BECB49EF76E948C4445AFF77EFFB4DEC6B"
         )
-        assert caajs[0].type == "lose"
+        assert caajs[0].type == "send"
         assert caajs[0].amount == "0.000049"
         assert caajs[0].token_symbol == "juno"
         assert (
@@ -127,17 +143,40 @@ class TestOsmosisPlugin:
         assert caajs[0].caaj_to == "juno14ls9rcxxd5gqwshj85dae74tcp3umyppqw2uq4"
         assert caajs[0].comment == ""
 
+    @classmethod
+    def get_token_table_mock(cls):
+        def mock_get_symbol(token_original_id: str) -> str:
+            if token_original_id is None:
+                return "osmo"
+            elif token_original_id == "ibc/46B44899322F3CD854D2D46DEEF881958467CDD4B3B10086DA49296BBED94BED":
+                return "juno"
+            elif token_original_id == "ibc/27394FB092D2ECCD56123C74F36E4C1F926001CEADA9CA97EA622B25F41E5EB2":
+                return "atom"
+            else:
+                return None
+
+        def mock_get_symbol_uuid(token_original_id: str) -> str:
+            return "3a2570c5-15c4-2860-52a8-bff14f27a236"
+
+        mock = MagicMock()
+        mock.get_symbol.side_effect = mock_get_symbol
+        mock.get_symbol_uuid.side_effect = mock_get_symbol_uuid
+        return mock
+
     def test_get_caaj_join_pool(self):
+
         test_data = TestOsmosisPlugin._get_test_data("join_pool")
         transaction = OsmosisTransaction(test_data)
+        mock = TestOsmosisPlugin.get_token_table_mock()
         caajs = OsmosisPlugin.get_caajs(
             "osmo14ls9rcxxd5gqwshj85dae74tcp3umypp786h3m",
             transaction,
+            mock
         )
         assert caajs[0].executed_at == "2022-01-21 02:50:09"
         assert caajs[0].chain == "osmosis"
         assert caajs[0].platform == "osmosis"
-        assert caajs[0].application == "lend"
+        assert caajs[0].application == "liquidity"
         assert (
             caajs[0].transaction_id
             == "266ADD2B17FCDD5BBEA5499EF863AC7463C45FB6F537E02FCCA8EE048255F4D2"
@@ -159,7 +198,7 @@ class TestOsmosisPlugin:
         assert caajs[1].executed_at == "2022-01-21 02:50:09"
         assert caajs[1].chain == "osmosis"
         assert caajs[1].platform == "osmosis"
-        assert caajs[1].application == "lend"
+        assert caajs[1].application == "liquidity"
         assert (
             caajs[1].transaction_id
             == "266ADD2B17FCDD5BBEA5499EF863AC7463C45FB6F537E02FCCA8EE048255F4D2"
@@ -178,7 +217,7 @@ class TestOsmosisPlugin:
         assert caajs[2].executed_at == "2022-01-21 02:50:09"
         assert caajs[2].chain == "osmosis"
         assert caajs[2].platform == "osmosis"
-        assert caajs[2].application == "lend"
+        assert caajs[2].application == "liquidity"
         assert (
             caajs[2].transaction_id
             == "266ADD2B17FCDD5BBEA5499EF863AC7463C45FB6F537E02FCCA8EE048255F4D2"
@@ -194,19 +233,21 @@ class TestOsmosisPlugin:
     def test_get_caaj_lock_token(self):
         test_data = TestOsmosisPlugin._get_test_data("lock_tokens")
         transaction = OsmosisTransaction(test_data)
+        mock = TestOsmosisPlugin.get_token_table_mock()
         caajs = OsmosisPlugin.get_caajs(
             "osmo14ls9rcxxd5gqwshj85dae74tcp3umypp786h3m",
             transaction,
+            mock
         )
         assert caajs[0].executed_at == "2022-01-21 02:52:07"
         assert caajs[0].chain == "osmosis"
         assert caajs[0].platform == "osmosis"
-        assert caajs[0].application == "lend"
+        assert caajs[0].application == "staking"
         assert (
             caajs[0].transaction_id
             == "3BA4AB4FCD0DB71F9A7239B55F1DD189DD38BFEEAF6EFFDA3BD4552DE6A3DADE"
         )
-        assert caajs[0].type == "deposit_liquidity"
+        assert caajs[0].type == "deposit"
         assert caajs[0].amount == "0.002"
         assert caajs[0].token_symbol is None
         assert caajs[0].token_original_id == "gamm/pool/497"
@@ -217,19 +258,21 @@ class TestOsmosisPlugin:
     def test_get_caaj_exit_pool(self):
         test_data = TestOsmosisPlugin._get_test_data("exit_pool")
         transaction = OsmosisTransaction(test_data)
+        mock = TestOsmosisPlugin.get_token_table_mock()
         caajs = OsmosisPlugin.get_caajs(
             "osmo14ls9rcxxd5gqwshj85dae74tcp3umypp786h3m",
             transaction,
+            mock
         )
         assert caajs[0].executed_at == "2022-01-21 02:54:12"
         assert caajs[0].chain == "osmosis"
         assert caajs[0].platform == "osmosis"
-        assert caajs[0].application == "lend"
+        assert caajs[0].application == "liquidity"
         assert (
             caajs[0].transaction_id
             == "8AA91C46E026F135C18BC37DA813A1682F283EFF7E5AE60899F740DE58470FBB"
         )
-        assert caajs[0].type == "withdraw_liquidity"
+        assert caajs[0].type == "withdraw"
         assert caajs[0].amount == "0.001382"
         assert caajs[0].token_symbol == "juno"
         assert (
@@ -246,12 +289,12 @@ class TestOsmosisPlugin:
         assert caajs[1].executed_at == "2022-01-21 02:54:12"
         assert caajs[1].chain == "osmosis"
         assert caajs[1].platform == "osmosis"
-        assert caajs[1].application == "lend"
+        assert caajs[1].application == "liquidity"
         assert (
             caajs[1].transaction_id
             == "8AA91C46E026F135C18BC37DA813A1682F283EFF7E5AE60899F740DE58470FBB"
         )
-        assert caajs[1].type == "withdraw_liquidity"
+        assert caajs[1].type == "withdraw"
         assert caajs[1].amount == "0.002678"
         assert caajs[1].token_symbol == "osmo"
         assert caajs[1].token_original_id is None
@@ -265,7 +308,7 @@ class TestOsmosisPlugin:
         assert caajs[2].executed_at == "2022-01-21 02:54:12"
         assert caajs[2].chain == "osmosis"
         assert caajs[2].platform == "osmosis"
-        assert caajs[2].application == "lend"
+        assert caajs[2].application == "liquidity"
         assert (
             caajs[2].transaction_id
             == "8AA91C46E026F135C18BC37DA813A1682F283EFF7E5AE60899F740DE58470FBB"
@@ -281,15 +324,17 @@ class TestOsmosisPlugin:
     def test_get_caaj_delegate(self):
         test_data = TestOsmosisPlugin._get_test_data("delegate")
         transaction = OsmosisTransaction(test_data)
+        mock = TestOsmosisPlugin.get_token_table_mock()
         caajs = OsmosisPlugin.get_caajs(
             "osmo14ls9rcxxd5gqwshj85dae74tcp3umypp786h3m",
             transaction,
+            mock
         )
 
         assert caajs[0].executed_at == "2022-01-31 09:15:23"
         assert caajs[0].chain == "osmosis"
         assert caajs[0].platform == "osmosis"
-        assert caajs[0].application == "lend"
+        assert caajs[0].application == "staking"
         assert (
             caajs[0].transaction_id
             == "04668DE27064363B86A1925F71B453C24B08862B5F5704399B6E478616874FED"
@@ -305,18 +350,22 @@ class TestOsmosisPlugin:
     def test_get_caajs_ibc_received_effect0(self):
         test_data = TestOsmosisPlugin._get_test_data("ibc_received_effect0")
         transaction = OsmosisTransaction(test_data)
+        mock = TestOsmosisPlugin.get_token_table_mock()
         caajs = OsmosisPlugin.get_caajs(
             "osmo14ls9rcxxd5gqwshj85dae74tcp3umypp786h3m",
             transaction,
+            mock
         )
         assert len(caajs) == 0
 
     def test_get_caajs_ibc_received_effect1(self):
         test_data = TestOsmosisPlugin._get_test_data("ibc_received_effect1")
         transaction = OsmosisTransaction(test_data)
+        mock = TestOsmosisPlugin.get_token_table_mock()
         caajs = OsmosisPlugin.get_caajs(
             "osmo14ls9rcxxd5gqwshj85dae74tcp3umypp786h3m",
             transaction,
+            mock
         )
 
         assert caajs[0].executed_at == "2022-01-20 06:39:04"
@@ -337,12 +386,6 @@ class TestOsmosisPlugin:
         assert caajs[0].caaj_from == "osmo1yl6hdjhmkf37639730gffanpzndzdpmhxy9ep3"
         assert caajs[0].caaj_to == "osmo14ls9rcxxd5gqwshj85dae74tcp3umypp786h3m"
         assert caajs[0].comment == ""
-
-    @classmethod
-    def _get_test_data(cls, filename):
-        with open(f"tests/data/{filename}.json", encoding="utf-8") as jsonfile_local:
-            test_data = json.load(jsonfile_local)
-        return test_data
 
     def test_get_token_amount(self):
         token_amount = OsmosisPlugin._get_token_amount(
@@ -389,36 +432,19 @@ class TestOsmosisPlugin:
             {"key": "amount", "value": "4323192512586978gamm/pool/497"},
         ]
 
-    def test_get_atoken_symbol_uosmo(self):
-        token_symbol = OsmosisPlugin._get_token_symbol("osmo")
-        assert token_symbol == "osmo"
+    @classmethod
+    def _get_test_data(cls, filename):
+        with open(f"tests/data/{filename}.json", encoding="utf-8") as jsonfile_local:
+            test_data = json.load(jsonfile_local)
+        return test_data
 
-    def test_get_atoken_symbol_uion(self):
-        token_symbol = OsmosisPlugin._get_token_symbol("ion")
-        assert token_symbol == "ion"
-
-    def test_get_atoken_symbol_atom(self):
-        token_symbol = OsmosisPlugin._get_token_symbol(
-            "ibc/27394FB092D2ECCD56123C74F36E4C1F926001CEADA9CA97EA622B25F41E5EB2"
+    @classmethod
+    def mock_get_token_original_ids(cls):
+        csv_file = open(
+            Path("%s/data/token_original_ids/token_original_id.csv" % os.path.dirname(__file__))
         )
-        assert token_symbol == "atom"
-
-    def test_get_atoken_symbol_nothing(self):
-        token_symbol = OsmosisPlugin._get_token_symbol("nothing_symbol_in_csv")
-        assert token_symbol is None
-
-    def test_get_symbol_uuid_uosmo(self):
-        symbol_uuid = OsmosisPlugin._get_symbol_uuid(None, "osmosis", "osmo")
-        assert symbol_uuid == "c0c8e177-53c3-c408-d8bd-067a2ef41ea7"
-
-    def test_get_symbol_uuid_juno(self):
-        symbol_uuid = OsmosisPlugin._get_symbol_uuid(
-            "ibc/46B44899322F3CD854D2D46DEEF881958467CDD4B3B10086DA49296BBED94BED",
-            "osmosis",
-            "juno",
-        )
-        assert symbol_uuid == "3a2570c5-15c4-2860-52a8-bff14f27a236"
-
+        csv_reader = csv.DictReader(csv_file)
+        return list(csv_reader)
 
 if __name__ == "__main__":
-    TestOsmosisPlugin.test_get_caaj_delegate(1)
+    TestOsmosisPlugin.test_get_caajs_ibc_received_effect1(1)
